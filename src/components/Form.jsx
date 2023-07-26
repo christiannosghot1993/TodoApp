7e2510793422dca6e5a8a4f418ef0dd98e4b699d
Csuarez ice <csuarez.ice@ibm.com>
2023-07-26 11:01:38 -0500

    Se añade diseño inicial de la app

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+
+export const Form = () => {
+    return (
+        <>
+            <h1 style={{ textAlign: 'center' }}>Lista de tareas</h1>
+            <form action="" className="container">
+                <div className="row">
+                    <input type="text" className="col-sm-10 styled-input" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    <button className="btn btn-primary col-sm-1">Crear</button>
+                </div>
+            </form>
+        </>
+    )
+}
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,16 @@
+
+export const TodoItem = () => {
+    return (
+        <div className="mt-3 d-flex tarjeta">
+            <div className="p-2 flex-grow-1">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, earum omnis culpa eius voluptatibus vitae saepe quam? Harum eius voluptatem, impedit omnis modi pariatur, placeat distinctio facilis fugiat dolor facere.</p>
+            </div>
+            <div className="p-2">
+                <button className="btn btn-danger">editar</button>
+            </div>
+            <div className="p-2">
+                <button className="btn btn-danger">editar</button>
+            </div>
+        </div>
+    )
+}
